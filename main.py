eb069a311a68aa65ba8670a3acc11c9432e5cdb6
nevidomyy <nevidom4ik@list.ru>
2023-06-20 18:55:02 +0300

    ...

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ import subprocess
 import argparse
 
 
-parser = argparse.ArgumentParser(description="nmap scanner")
-parser.add_argument('-ip_filename', dest='ip_filename', metavar='ip_filename', required=True,
-                    help='IPs file path to scan (.txt)')
-parser.add_argument('-port_filename', dest='port_filename', metavar='port_filename', required=True,
-                    help='Ports file path to scan (.txt)')
-args = parser.parse_args()
-
-
 async def nmap_ip_scan(ip_address: str) -> str:
     process = await asyncio.create_subprocess_shell(f'nmap {ip_address}', stdout=subprocess.PIPE)
     output, _ = await process.communicate()
@@ -41,4 +33,11 @@ async def scan_all_ips(ip_file_path: str, port_file_path: str):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="nmap scanner")
+    parser.add_argument('-ip_filename', dest='ip_filename', metavar='ip_filename', required=True,
+                        help='IPs file path to scan (.txt)')
+    parser.add_argument('-port_filename', dest='port_filename', metavar='port_filename', required=True,
+                        help='Ports file path to scan (.txt)')
+
+    args = parser.parse_args()
     asyncio.run(scan_all_ips(args.ip_filename, args.port_filename))
